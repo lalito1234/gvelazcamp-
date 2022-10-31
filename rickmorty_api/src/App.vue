7b2c6077f3dfaebcd9cd0038e324ba63532128de
lalito1234 <115757893+lalito1234@users.noreply.github.com>
2022-10-30 21:49:02 -0300

    Update App.vue

--- rickmorty_api/src/App.vue
+++ rickmorty_api/src/App.vue
@@ -12,7 +12,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 nav {
@@ -21,7 +21,7 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 nav a.router-link-exact-active {
